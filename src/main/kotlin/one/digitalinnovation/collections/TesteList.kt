6ef2d1d795fbbd65ad645e50d315fe82f3e4b8d5
Paulo Eduardo Cruz de Lemos Junior <pauloeduardo2906@gmail.com>
2021-08-28 00:53:40 -0300

    created operations of groupBy

--- src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,28 +1,47 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val kyle = Funcionario("Kyle Ren", 2000.0)
-    val anakin = Funcionario("Anakin Skywalker", 5000.0)
-    val obiwan = Funcionario("Obi-Wan Kenobi", 9000.0)
+    val kyle = Funcionario("Kyle Ren", 3000.0, "CLT")
+    val anakin = Funcionario("Anakin Skywalker", 7000.0, "PJ")
+    val obiwan = Funcionario("Obi-Wan Kenobi", 1500.0, "PJ")
 
     val funcionarios = listOf(kyle, anakin, obiwan)
+
+    println("\n -------------------- \n")
 
     funcionarios.forEach {
         println(it)
     }
 
+    println("\n -------------------- \n")
+
     println(funcionarios.find {it.nome == "Kyle Ren"})
+
+
+    println("\n -------------------- \n")
+
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it) }
+
+
+    println("\n -------------------- \n")
+
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach { println(it) }
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ){
     override fun toString(): String =
         """
             Nome:    $nome
             Salário: $salario
-            -----------------
+            
         """.trimIndent()
 
 }
